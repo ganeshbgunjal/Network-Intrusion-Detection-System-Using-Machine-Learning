9f6ef2cc507a21489319ea78991d016eba16c415
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2026-03-17 18:34:23 +0530

    Updated gitignore and added mongodb test file

--- test_mongodb.py
+++ test_mongodb.py
@@ -13,23 +13,3 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
-
-# username==ganeshgunjal118_db_user
-# passwrd==YY97ix6GSpnPO0dH
-
-
-
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-
-# uri = "mongodb+srv://ganeshgunjal118_db_user:Admin123@cluster01.rwkvxvz.mongodb.net/?appName=Cluster01"
-
-# # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
